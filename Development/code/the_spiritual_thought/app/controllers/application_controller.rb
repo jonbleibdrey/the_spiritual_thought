6ecb37abb8e318e043f6e5c137a9262ff6069e1c
jonbleibdrey <jonbleibdrey@aol.com>
2020-07-14 18:14:25 -0400

    create,update created

--- Development/code/the_spiritual_thought/app/controllers/application_controller.rb
+++ Development/code/the_spiritual_thought/app/controllers/application_controller.rb
@@ -2,13 +2,12 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do   
-    enable :sessions
-  end
-
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret,"hello"
   end
 
    get "/" do
@@ -24,7 +23,7 @@ class ApplicationController < Sinatra::Base
   def current_user
     User.find(session[:user_id])
   end
-  
+
   end
 
 end
--- Development/code/the_spiritual_thought/app/controllers/journal_controller.rb
+++ Development/code/the_spiritual_thought/app/controllers/journal_controller.rb
@@ -1,8 +1,39 @@
 class JournalController < ApplicationController
 
+
+    #read
     get '/journal/index' do
-        @journals = Journal.all
+        @journal = current_user.journals
         erb :'/journal/index'
     end
+
+    # get '/journal/all' do
+    #     @journals = Journal.all
+    #     erb :'/journal/all'
+    # end
+
+    #create
+    get '/journal/new' do
+        erb :'/journal/new'
+    end
+
+    post '/journal/new' do
+        @journal = current_user.journals.create(title: params[:title], content: params[:content])
+        redirect to :'/journal/index'
+    end
+
+    #update
+    get '/journal/:id/edit' do
+        @journal = Journal.find_by(id: params[:id])
+        erb :'/journal/edit'
+    end
+
+    patch '/journal/:id' do
+        @journal = Journal.find_by(id: params[:id]).update(title: params[:title], content: params[:content])
+        redirect to :'/journal/index'
+    end
+
+    #delete
+
 
 end
